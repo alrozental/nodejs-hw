7e3f2cb169d3c9415caef718fcce47e574885048
Alina Rozental <dev.alinaro@gmail.com>
2026-06-01 18:06:45 +0200

    done 3

--- src/controllers/notesController.js
+++ src/controllers/notesController.js
@@ -1,75 +1,46 @@
+import createError from 'http-errors';
 import { Note } from '../models/note.js';
 
 export const getAllNotes = async (req, res) => {
-  try {
-    const notes = await Note.find();
-    if (!notes) {
-      return res.status(404).json({ message: 'Notes not found' });
-    }
-    res.status(200).json(notes);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal Server Error', error: error.message });
+  const notes = await Note.find();
+  if (!notes || notes.length === 0) {
+    throw createError(404, 'Notes not found');
   }
+  res.status(200).json(notes);
 };
 
 export const getNoteById = async (req, res) => {
-  try {
-    const note = await Note.findById(req.params.noteId);
-    if (!note) {
-      return res.status(404).json({ message: 'Note not found' });
-    }
-    res.status(200).json(note);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal Server Error', error: error.message });
+  const note = await Note.findById(req.params.noteId);
+  if (!note) {
+    throw createError(404, 'Note not found');
   }
+  res.status(200).json(note);
 };
 
 export const createNote = async (req, res) => {
-  try {
-    const { title, content } = req.body;
-    const newNote = new Note({ title, content });
-    const savedNote = await newNote.save();
-    res.status(201).json(savedNote);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal Server Error', error: error.message });
-  }
+  const { title, content, tag } = req.body;
+  const newNote = new Note({ title, content, tag });
+  const savedNote = await newNote.save();
+  res.status(201).json(savedNote);
 };
 
 export const deleteNote = async (req, res) => {
-  try {
-    const deletedNote = await Note.findByIdAndDelete(req.params.noteId);
-    if (!deletedNote) {
-      return res.status(404).json({ message: 'Note not found' });
-    }
-    res.status(200).json({ message: 'Note deleted successfully' });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal Server Error', error: error.message });
+  const deletedNote = await Note.findByIdAndDelete(req.params.noteId);
+  if (!deletedNote) {
+    throw createError(404, 'Note not found');
   }
+  res.status(200).json(deletedNote);
 };
 
 export const updateNote = async (req, res) => {
-  try {
-    const { title, content } = req.body;
-    const updatedNote = await Note.findByIdAndUpdate(
-      req.params.noteId,
-      { title, content },
-      { new: true },
-    );
-    if (!updatedNote) {
-      return res.status(404).json({ message: 'Note not found' });
-    }
-    res.status(200).json(updatedNote);
-  } catch (error) {
-    res
-      .status(500)
-      .json({ message: 'Internal Server Error', error: error.message });
+  const { title, content, tag } = req.body;
+  const updatedNote = await Note.findByIdAndUpdate(
+    req.params.noteId,
+    { title, content, tag },
+    { returnDocument: 'after' },
+  );
+  if (!updatedNote) {
+    throw createError(404, 'Note not found');
   }
+  res.status(200).json(updatedNote);
 };
--- src/server.js
+++ src/server.js
@@ -3,11 +3,9 @@ import cors from 'cors';
 import 'dotenv/config';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
-
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
-
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
@@ -16,7 +14,7 @@ const PORT = process.env.PORT || 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
-app.use('/notes', notesRoutes);
+app.use(notesRoutes);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
