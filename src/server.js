84128c76fdc6c6c93090e7381e0614eef7d973d2
Alina Rozental <dev.alinaro@gmail.com>
2026-06-23 11:10:58 +0200

    done

--- src/server.js
+++ src/server.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import cors from 'cors';
 import 'dotenv/config';
-import { errors } from "celebrate";
+import { errors } from 'celebrate';
 
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
+import authRoutes from './routes/authRoutes.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -15,7 +17,9 @@ const PORT = process.env.PORT || 3000;
 app.use(logger);
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(notesRoutes);
+app.use(authRoutes);
 app.use(errors());
 app.use(notFoundHandler);
 app.use(errorHandler);
