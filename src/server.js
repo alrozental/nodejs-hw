d255d3460f907c1ff97011b121ca12b43b9f6e08
Alina Rozental <dev.alinaro@gmail.com>
2026-06-24 10:32:19 +0200

    done

--- src/server.js
+++ src/server.js
@@ -9,6 +9,7 @@ import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import notesRoutes from './routes/notesRoutes.js';
 import authRoutes from './routes/authRoutes.js';
+import userRoutes from './routes/userRoutes.js';
 import cookieParser from 'cookie-parser';
 
 const app = express();
@@ -20,6 +21,7 @@ app.use(cors());
 app.use(cookieParser());
 app.use(notesRoutes);
 app.use(authRoutes);
+app.use(userRoutes);
 app.use(errors());
 app.use(notFoundHandler);
 app.use(errorHandler);
