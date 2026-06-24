69ac21c1357257a0339363dddd668518aa8902a5
Alina Rozental <dev.alinaro@gmail.com>
2026-06-24 10:54:38 +0200

    fix: adjust middleware order for errors and notFoundHandler

--- src/server.js
+++ src/server.js
@@ -22,8 +22,9 @@ app.use(cookieParser());
 app.use(notesRoutes);
 app.use(authRoutes);
 app.use(userRoutes);
-app.use(errors());
+
 app.use(notFoundHandler);
+app.use(errors());
 app.use(errorHandler);
 
 await connectMongoDB();
